840550b1b8959e297ca4ffed5585492f25334ee2
kei-cs <keisuke.cs@gmail.com>
2019-02-14 02:10:16 +0900

    Demonstrate how to get boards from set user

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
   // "os"
   "fmt"
-
+  "github.com/adlio/trello"
   "github.com/mitchellh/go-homedir"
   "github.com/pelletier/go-toml"
 )
@@ -12,5 +12,13 @@ func main() {
   home, _ := homedir.Dir()
   fmt.Println(home)
   config, _ := toml.LoadFile(home + "/.perica")
-  fmt.Println(config.Get("username").(string))
+  app_key := config.Get("app_key").(string)
+  token := config.Get("token").(string)
+  username := config.Get("username").(string)
+
+  client := trello.NewClient(app_key, token)
+
+  member, _ := client.GetMember(username, trello.Defaults())
+  boards, _ := member.GetBoards(trello.Defaults())
+
 }
