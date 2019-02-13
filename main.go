489f6d4bce55fd95fc2b1aca2dbe9790e0e3fed5
kei-cs <keisuke.cs@gmail.com>
2019-02-14 02:37:25 +0900

    Tried to add card to a list

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
   "github.com/adlio/trello"
   "github.com/mitchellh/go-homedir"
   "github.com/pelletier/go-toml"
+  // inspect "github.com/vigo/go-inspect"
 )
 
 func main() {
@@ -14,11 +15,24 @@ func main() {
   config, _ := toml.LoadFile(home + "/.perica")
   app_key := config.Get("app_key").(string)
   token := config.Get("token").(string)
-  username := config.Get("username").(string)
+  // username := config.Get("username").(string)
 
   client := trello.NewClient(app_key, token)
 
-  member, _ := client.GetMember(username, trello.Defaults())
-  boards, _ := member.GetBoards(trello.Defaults())
+  // member, _ := client.GetMember(username, trello.Defaults())
+
+  // Print Trelli lists
+  // board, _ := client.GetBoard("rGH9kMUZ", trello.Defaults())
+  // lists, _ := board.GetLists(trello.Defaults())
+  // for _, element := range(lists) {
+  //   fmt.Println(inspect.Element(element))
+  // }
+
+  card := trello.Card{
+    Name: "Card Name",
+    Desc: "Card description",
+  }
+  list, _ := client.GetList("5c6450f088729a297d8dacfd", trello.Defaults())
+  list.AddCard(&card, trello.Defaults())
 
 }
