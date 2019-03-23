59b5b0591ae9e3a73ba40833d12ee0ca8b315a10
kei-cs <keisuke.cs@gmail.com>
2019-03-23 13:12:40 +0900

    Implemented perica command finally

---
+++ cmd/root.go
@@ -0,0 +1,35 @@
+package cmd
+
+import (
+  "fmt"
+  "github.com/spf13/cobra"
+  "os"
+  "log"
+  "github.com/garigari-kun/perica/client"
+)
+
+func RootCmd() *cobra.Command {
+  cmd := &cobra.Command{
+    Use:   "perica",
+    Short: "Issue to Trello card.",
+    Run: func(cmd *cobra.Command, args []string) {
+      if len(args) < 1 {
+        log.Fatal("You need to parse issue url!")
+      }
+      input_url := args[0]
+      github_client := client.NewGitHubClient(input_url)
+      title, body := github_client.GetIssueTitleAndBody(input_url)
+      client := client.NewTrelloClient()
+      client.CreateCard(title, body)
+    },
+  }
+  return cmd
+}
+
+func Execute() {
+  cmd := RootCmd()
+  if err := cmd.Execute(); err != nil {
+    log.Print(err)
+    os.Exit(1)
+  }
+}
--- main.go
+++ main.go
@@ -3,9 +3,14 @@ package main
 import (
   "github.com/garigari-kun/perica/client"
   // inspect "github.com/vigo/go-inspect"
+  "github.com/garigari-kun/perica/cmd"
 )
 
 func main() {
+  cmd.Execute()
+}
+
+func _main() {
   github_client := client.NewGitHubClient("https://github.com/garigari-kun/shutto/issues/5")
   title, body := github_client.GetIssueTitleAndBody("https://github.com/garigari-kun/shutto/issues/5")
   client := client.NewTrelloClient()
