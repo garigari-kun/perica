d512710d772e6dc4f1e26f6a5f208240b88c2768
kei-cs <keisuke.cs@gmail.com>
2019-03-23 13:13:29 +0900

    Cleaned main.go

--- main.go
+++ main.go
@@ -9,12 +9,4 @@ import (
 func main() {
   cmd.Execute()
 }
-
-func _main() {
-  github_client := client.NewGitHubClient("https://github.com/garigari-kun/shutto/issues/5")
-  title, body := github_client.GetIssueTitleAndBody("https://github.com/garigari-kun/shutto/issues/5")
-  client := client.NewTrelloClient()
-  client.CreateCard(title, body)
-}
-
 // You can get app-key and token from here https://trello.com/app-key
